53075f7c695d8d9d37dae4f6805ef93a325cf83c
ccampanha <cristiana.campanha@gmail.com>
2021-09-12 19:47:43 -0300

    estilizacao do modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global"
+import { useState } from "react";
+import Modal from "react-modal"
+import { NewTransactionModal } from "./components/NewTransactionModal";
+Modal.setAppElement("#root") //informa a #root por acessibilisdade avisa que a root não está visivelporque o Modal está acina
+
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionModalOpen(false)
+  }
   return (
     <>
-      <Header />
-      <Dashboard /> 
+      <Header  onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <Dashboard />
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />         
     </>
   );
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,7 +7,7 @@ export const GlobalStyle = createGlobalStyle`
     --green: #33cc95;
     --blue: #5429cc;
     --blue-light: #6933ff;
-    --text-title: #363ff5;
+    --text-title: #363F5F;
     --text-body: #969cb3;
     --shape: #FFFFFF;
 
@@ -52,4 +52,39 @@ export const GlobalStyle = createGlobalStyle`
     font-weight: 600;
   }
 
+.react-modal-overlay{
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.react-modal-content{
+  width: 100%;
+  max-width:576px;
+  background: var(--background);
+  padding: 3rem;
+  position:relative;
+  border-radius: 0.24rem;
+}
+
+.react-modal-close {
+  position: absolute;
+  right: 1.5rem;
+  top: 1.5rem;
+  border: 0;
+  background: transparent;
+
+  transition: filter 0.2s;
+
+  &:hover {
+    filter: brightness(0.8);
+  }
+}
 `
